4604ca83fbb0c7bdc1428ba173c1ebcc5e6da279
KG <69757237+kunalg12@users.noreply.github.com>
2022-02-15 08:11:50 +0530

    Add files via upload

---
+++ Linked list/doublyclg.c
@@ -0,0 +1,128 @@
+#include<stdio.h>
+#include<stdlib.h>
+ struct node
+  {
+   int data;
+   struct node *next;
+   struct node *prev;
+  }*newnode,*head,*tail;
+ void create()
+  {
+    char ch;
+    do
+    {
+     newnode=(struct node*)malloc(sizeof(struct node));
+     printf("Enter the data");
+     scanf("%d",&newnode->data);
+     newnode->prev=NULL;
+     newnode->next=NULL;
+     if (head == NULL)
+      {
+	head=newnode;
+	tail=newnode;
+      }
+     else
+     {
+       tail->next=newnode;
+       newnode->prev=tail;
+       tail=newnode;
+     }
+     printf("Do you want to create another \n");
+     ch=getche();
+    }while(ch!='n');
+  }
+  void display()
+  {
+   struct node *temp;
+   printf("The linked list is");
+   temp=head;
+   while(temp!=NULL)
+    {
+     printf("%3d",temp->data);
+     temp=temp->next;
+    }
+  }
+  void insert_at_begin()
+  {
+   newnode=(struct node*)malloc(sizeof(struct node));
+   printf(" Enter the data");
+   scanf("%d",&newnode->data);
+   head->prev=newnode;
+   newnode->next=head;
+   head=newnode;
+  }
+  void insert_at_end()
+  {
+   newnode=(struct node*)malloc(sizeof(struct node));
+   printf(" Enter the data");
+   scanf("%d",&newnode->data);
+   tail->next=newnode;
+   newnode->prev=tail;
+   tail=newnode;
+  }
+  void insert_at_pos()
+  {
+   int i=1,count=0,pos;
+   struct node *temp;
+   temp=head;
+   while(temp!=NULL)
+    {
+      count++;
+      temp=temp->next;
+    }
+    printf("the length is %d \n ",count);
+    printf("Enter the position \n");
+    scanf("%d",&pos);
+ if(pos>count)
+   {
+      printf("Invallid position");
+   }
+   else   {
+  temp=head;
+    while(i<(pos-1))
+   {
+    temp=temp->next;
+    i++;
+   }
+   newnode=(struct node*)malloc(sizeof(struct node));
+   printf(" Enter the data");
+   scanf("%d",&newnode->data);
+   newnode->prev=temp;
+   newnode->next=temp->next;
+   temp->next=newnode;
+   newnode->next->prev=newnode;
+   }
+  }
+
+ void main()
+ {
+  int no=1;
+  clrscr();
+  while(no!=4)
+  {
+   printf("\n1.Create\n");
+   printf("2.display \n");
+   printf("3.Insert at begin \n");
+   printf("4.insert at end \n");
+   printf("5 Insert at pos \n");
+
+   printf("enter your choice");
+   scanf("%d",&no);
+   switch (no)
+    {
+      case 1: create();
+	      break;
+      case 2: display();
+	      break;
+     case 3: insert_at_begin();
+	      break;
+    case 4: insert_at_end();
+	    break;
+      case 5:insert_at_pos();
+	      break;
+      case 6: exit(0);
+	      break;
+      default:printf("invalid input");
+    }
+  }
+ }
